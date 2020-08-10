a97b18c73142c0c9634a0ee1290e0b98bc97f12f
Brian Gonzalez <developer.briangonzalez@gmail.com>
2020-08-09 21:20:49 -0500

    Removes Users component

--- client/client.tsx
+++ client/client.tsx
@@ -3,14 +3,12 @@ import ReactDOM from 'react-dom';
 import {Admin, Resource} from 'react-admin';
 
 import dataProvider from './data-provider';
-import {UserList} from './components/users';
 
 const App = () =>
     <Admin
         dataProvider={dataProvider}
     >
         <Resource
-            list={UserList}
             name="users"
         />
     </Admin>;
