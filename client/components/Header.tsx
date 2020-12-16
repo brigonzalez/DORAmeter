ed275cbd69b37fb23cea1249dc03599264f98d16
Brian Gonzalez <developer.briangonzalez@gmail.com>
2020-12-15 20:07:37 -0700

    SVG >>>>>>>>>>>>>>>>>>>> PNG

--- client/components/Header.tsx
+++ client/components/Header.tsx
@@ -3,12 +3,13 @@ import React from 'react';
 import '../css/header.scss';
 
 // @ts-ignore
-import DORAmeterLogo from '../assets/DORAmeter-logo.png';
+import DORAmeterLogo from '../assets/DORAmeter-logo.svg';
 
 const Header = () =>
     <div className={'header'}>
         <img
             alt={'DORAmeter logo'}
+            className={'dorameter-logo'}
             src={DORAmeterLogo}
         />
     </div>;
