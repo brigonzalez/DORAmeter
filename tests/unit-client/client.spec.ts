bc8a171cf37f0e2def9838aa29ad6a857ee350f9
Brian Gonzalez <developer.briangonzalez@gmail.com>
2020-12-16 21:04:18 -0700

    Addresses client.ts tests

--- tests/unit-client/client.spec.ts
+++ tests/unit-client/client.spec.ts
@@ -1,13 +1,22 @@
 import Chance from 'chance';
 import ReactDOM from 'react-dom';
 
+import Header from '../../client/components/Header';
+
 jest.mock('react-dom');
+jest.mock('../../client/components/Header');
 
 describe('client', () => {
     const chance = new Chance();
 
-    let expectedApp,
-        expectedSelectedDOMElement;
+    let expectedSelectedDOMElement: string,
+        expectedRoot: any,
+        expectedHeader: any;
+
+    const cacheChildren = (expectedApp: any) => {
+        expectedRoot = expectedApp.type();
+        expectedHeader = expectedRoot.props.children;
+    };
 
     beforeAll(() => {
         expectedSelectedDOMElement = chance.string();
@@ -15,21 +24,25 @@ describe('client', () => {
 
         require('../../client/client');
 
-        expectedApp = ReactDOM.render.mock.calls[0][0];
+        const expectedApp = (ReactDOM.render as jest.Mock).mock.calls[0][0];
+
+        cacheChildren(expectedApp);
     });
 
     describe('react-admin component', () => {
-        test('should do render Admin component from react-admin', () => {
-            const adminComponent = expectedApp.type();
-            const resourceComponent = adminComponent.props.children;
+        test('should render div as root component', () => {
+            expect(expectedRoot.type).toBe('div');
+            expect(expectedRoot.props.className).toBe('app');
+        });
 
-            expect(resourceComponent.props.name).toBe('users');
+        test('should render Header as child of div root component', () => {
+            expect(expectedHeader.type).toBe(Header);
         });
     });
 
     describe('selected element for rendering DOM', () => {
         test('should select the correct component for rendering', () => {
-            expect(ReactDOM.render.mock.calls[0][1]).toBe(expectedSelectedDOMElement);
+            expect((ReactDOM.render as jest.Mock).mock.calls[0][1]).toBe(expectedSelectedDOMElement);
             expect(document.querySelector).toHaveBeenCalledTimes(1);
             expect(document.querySelector).toHaveBeenCalledWith('#app');
         });
