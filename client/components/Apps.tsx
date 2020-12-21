ea849b4bc2cc97714fabf5ba5a53ec6cc7b6fdc8
Brian Gonzalez <developer.briangonzalez@gmail.com>
2020-12-20 21:22:21 -0700

    Fixes problem of having just one app with data

--- client/components/Apps.tsx
+++ client/components/Apps.tsx
@@ -17,7 +17,6 @@ const Apps = () => {
                             name
                             deploymentFrequency {
                                 rating
-                                lastDeploymentTimestamp
                             }
                         }
                     }
